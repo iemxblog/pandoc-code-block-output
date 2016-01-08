56c9389a111a4a331e1d6ccb65d621712bc07f55
iemxblog <iemxblog@gmail.com>
2016-01-08 18:21:08 +0100

    Better temp file, using 'temporary' package

--- Main.hs
+++ Main.hs
@@ -6,6 +6,9 @@ import Text.Pandoc.JSON
 import Text.Pandoc
 import System.Process
 import Data.List
+import System.IO.Temp
+import System.IO
+import System.Directory
 
 
 -- | Merges the code block and its output into a Pandoc 'Block'
@@ -22,9 +25,12 @@ output :: Block -> IO Block
 output cb@(CodeBlock (_, ["haskell"], namevals) xs) = 
     case lookup "runWith" namevals of
         Just c -> do
-            writeFile "out.hs" xs
+            (tmpFile, hFile) <- openTempFile "./" "*.hs"
+            hPutStr hFile xs
+            hClose hFile
             let ([cName], params) = splitAt 1 $ words c
-            out <- readProcess cName (params ++ ["out.hs"]) []
+            out <- readProcess cName (params ++ [tmpFile]) []
+            removeFile tmpFile
             return $ combine cb out
         Nothing -> return cb
 output x = return x
